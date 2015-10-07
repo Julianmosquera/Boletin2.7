fd2b82a4e9ce3b17414d622867f4ac6ff1ce229c
julianlinux <julianlinux@julianlinux-X555LA>
2015-10-08 01:01:37 +0200

    k

---
+++ src/bol27/Bol27.java
@@ -0,0 +1,31 @@
+
+package bol27;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author julianlinux
+ */
+public class Bol27 {
+
+    
+    public static void main(String[] args) {
+        float n,i,t;
+        System.out.println("Introduce el nominal");
+        Scanner dato=new Scanner (System.in);
+        n=dato.nextFloat();
+        System.out.println("Introduce el interes");
+        i=dato.nextFloat();
+        System.out.println("Inserte el tiempo");
+        t=dato.nextFloat();
+        
+        t=n*i*t/1200;
+        
+        System.out.println("Renta mensual="+t);
+        
+        
+        
+    }
+    
+}
